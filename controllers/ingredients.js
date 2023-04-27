7d1061f209a5a6407ab10a5f343e07cb59234273
dangabrielle <hoangabrielle@gmail.com>
2023-04-26 14:32:21 -1000

    add preview functionality of shopping list

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -2,34 +2,6 @@ const Recipe = require("../models/recipe");
 const Ingredient = require("../models/ingredient");
 const ShoppingList = require("../models/shopping-list");
 
-// async function create(req, res) {
-//   try {
-//     // const ingredient = req.body;
-//     // ingredient.ingredientName = req.params.id;
-
-//     await Ingredient.create(req.body);
-//     res.redirect("ingredients/new");
-//   } catch (error) {
-//     console.log(error);
-//     res.render("error", { title: "Something went wrong" });
-//   }
-// }
-
-// async function newIngredient(req, res) {
-//   try {
-//     const foundIngredient = await Ingredient.find({}).populate(
-//       "ingredientName"
-//     );
-//     res.render("ingredients/new", {
-//       ingredient: foundIngredient,
-//       title: "Enter a new ingredient",
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.render("error", { title: "Something went wrong" });
-//   }
-// }
-
 async function create(req, res) {
   try {
     const newItem = await ShoppingList.create(req.body);
