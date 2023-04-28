0c47c5282919f305d379c0ea6482d590d87c8f6f
dangabrielle <hoangabrielle@gmail.com>
2023-04-27 17:12:19 -1000

    fixed preview padding

--- models/ingredient.js
+++ models/ingredient.js
@@ -6,13 +6,7 @@ const ingredientSchema = new Schema(
   {
     ingredientName: {
       type: Array,
-      // required: true,
-      // unique: true,
     },
-    // quantity: {
-    //   type: Number,
-    // min: 1,
-    // required: true,
   },
 
   { timestamps: true }
